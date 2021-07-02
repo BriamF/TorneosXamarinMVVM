68e5d7b4d0d02c15136fabb0393e27a9f41f6b26
BriamF <briam.flores11@gmail.com>
2021-07-02 13:22:16 -0500

    First Commit

---
+++ Torneos/VistaModelos/TorneoVistaModelo.cs
@@ -0,0 +1,30 @@
+﻿using Torneos.VistaModelos;
+
+namespace Torneos.VistaModelos
+{
+    public class TorneoVistaModelo : BaseVistaModelo
+    {
+        #region Constructor
+        public TorneoVistaModelo()
+        {
+
+        }
+        #endregion
+
+        #region Atributos
+
+        #endregion
+
+        #region Propiedades
+
+        #endregion
+
+        #region Comandos
+
+        #endregion
+
+        #region Metodos
+
+        #endregion
+    }
+}
